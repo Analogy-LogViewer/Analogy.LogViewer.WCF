ffb12ee57c864e3e65c21fae4dd6192fd2a9a3de
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-04-14 13:14:23 +0300

    update interface

--- Analogy.LogViewer.WCF/WCFServices/AnalogyClientSender.cs
+++ Analogy.LogViewer.WCF/WCFServices/AnalogyClientSender.cs
@@ -1,6 +1,7 @@
 ﻿using Analogy.Interfaces;
 using Analogy.LogViewer.WCF.WCFServicesInfrastructure;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Analogy.LogViewer.WCF.WCFServices
 {
@@ -10,6 +11,11 @@ namespace Analogy.LogViewer.WCF.WCFServices
         private static string clientConfigFile = "Analogy.LogViewer.WCF.dll.config";
         public AnalogyClientSender(string serverIP, string port) : base(clientEndpointConfigurationSectionName, clientConfigFile, $"http://{serverIP}:{port}/AnalogyService")
         {
+        }
+
+        public Task<bool> InitializeSender()
+        {
+            throw new System.NotImplementedException();
         }
 
         public void SendMessage(AnalogyLogMessage message, string source)
@@ -25,6 +31,11 @@ namespace Analogy.LogViewer.WCF.WCFServices
         public void SendMessages(byte[] messages, string source)
         {
             ClientProxy.SendMessages(messages, source);
+        }
+
+        public Task<bool> CleanupSender()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
