150fb96a5eb190e4fef8aede545758747f86e144
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-04-17 19:51:43 +0300

    factory

---
+++ Analogy.LogViewer.WCF/IAnalogy/ShareableFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analogy.Interfaces;
+using Analogy.Interfaces.Factories;
+
+namespace Analogy.LogViewer.WCF.IAnalogy
+{
+    public class ShareableFactory:IAnalogyShareableFactory
+    {
+        public Guid FactoryId { get; } = WcfFactory.Id;
+        public string Title { get; } = "WCF Share";
+        public IEnumerable<IAnalogyShareable> Shareables { get; }=new List<IAnalogyShareable>{new ShareableComponent()};
+    }
+}
