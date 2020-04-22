600cfe4ce96622bb6589fa55a4008b0234327364
Lior Banai <liorbanai@gmail.com>
2020-04-22 22:01:32 +0300

    update

--- Analogy.LogViewer.WCF/IAnalogy/ShareableComponent.cs
+++ Analogy.LogViewer.WCF/IAnalogy/ShareableComponent.cs
@@ -1,39 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Analogy.Interfaces;
+﻿using Analogy.Interfaces;
 using Analogy.LogViewer.WCF.Managers;
 using Analogy.LogViewer.WCF.WCFServices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Analogy.LogViewer.WCF.IAnalogy
 {
-    public class ShareableComponent: IAnalogyShareable
+    public class ShareableComponent : IAnalogyShareable
     {
+
+        public string OptionalTitle { get; set; } = "Analogy WCF share";
         private AnalogyClientSender proxy;
 
         public Task<bool> InitializeSender()
         {
-            proxy=new AnalogyClientSender(UserSettingsManager.UserSettings.Settings.IP, UserSettingsManager.UserSettings.Settings.Port);
-            
+            proxy = new AnalogyClientSender(UserSettingsManager.UserSettings.Settings.IP, UserSettingsManager.UserSettings.Settings.Port);
+
             proxy.Connect();
             return Task.FromResult(true);
         }
 
         public void SendMessage(AnalogyLogMessage message, string source)
         {
-            proxy.SendMessage(message,source);
+            proxy.SendMessage(message, source);
         }
 
-        public void SendMessages(IEnumerable<AnalogyLogMessage> messages, string source)
+        public void SendMessages(List<AnalogyLogMessage> messages, string source)
         {
-            proxy.SendMessages(messages.ToList(),source);
+            proxy.SendMessages(messages, source);
         }
 
         public void SendMessages(byte[] messages, string source)
         {
-            proxy.SendMessages(messages,source);
+            proxy.SendMessages(messages, source);
         }
 
         public Task<bool> CleanupSender()
@@ -42,5 +41,6 @@ namespace Analogy.LogViewer.WCF.IAnalogy
             proxy.Dispose();
             return Task.FromResult(true);
         }
+
     }
 }
